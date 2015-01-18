fc442e32df1d737759e9cb4ff06c211229cb6ba4
ruiandersonsantos <ruiandersonsantos@gmail.com>
2015-01-18 20:42:35 -0200

    Alteração da classe DatabaseHelper 18/01/2015

--- app/src/main/java/dao/DatabaseHelper.java
+++ app/src/main/java/dao/DatabaseHelper.java
@@ -26,7 +26,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
         // Criando tabela de tarefas.
         String sqlTarefa = "create table tarefas(_id integer primary key autoincrement," +
-                "tarefa text not null, dt_criacao date not null, dt_completado)";
+                "tarefa text not null, dt_criacao datetime default current_datetime, dt_completado)";
         db.execSQL(sqlTarefa);
 
     }
@@ -38,9 +38,25 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     public static class Usuarios{
         public static final String TABELA = "usuarios";
+        public static final String _ID = "_id";
+        public static final String NOME = "nome";
+        public static final String LOGIN = "login";
+        public static final String SENHA = "senha";
+
+        public static final String[] COLUNAS = new String[]{
+                _ID, NOME, LOGIN, SENHA
+        };
     }
 
     public static class Tarefas{
         public static final String TABELA = "tarefas";
+        public static final String _ID = "_id";
+        public static final String TAREFA = "tarefa";
+        public static final String DT_CRIACAO = "dt_criacao";
+        public static final String DT_COMPLETADO = "dt_completado";
+
+        public static final String[] COLUNAS = new String[]{
+                _ID, TAREFA, DT_CRIACAO, DT_COMPLETADO
+        };
     }
 }
